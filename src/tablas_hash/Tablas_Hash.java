f53f5f714fa834b99e65886604f90bc0be3da174
oliver <oliverjorgen555@gmail.com>
2022-05-01 11:05:57 -0600

    1

---
+++ src/tablas_hash/Tablas_Hash.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tablas_hash;
+import Vista.*;
+/**
+ *
+ * @author OLIVER
+ */
+public class Tablas_Hash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_HashForm Hash = new frm_HashForm();
+        Hash.show();
+        
+    }
+    
+}
